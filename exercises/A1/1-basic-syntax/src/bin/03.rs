835d6b02a2ce1dbc7f8c4a6159848c0aacafcc02
Gail Wadleigh <89820427+moschams@users.noreply.github.com>
2023-08-01 15:03:38 +0000

    add A1

--- exercises/A1/1-basic-syntax/src/bin/03.rs
+++ exercises/A1/1-basic-syntax/src/bin/03.rs
@@ -1,7 +1,15 @@
 fn main() {
     let input = [23, 82, 16, 45, 21, 94, 12, 34];
 
-    // TODO
+    let a = match input.iter().max() {
+        Some(num) => num, // 如果是 Some，就返回里面的值
+        None => panic!("input is empty"), // 如果是 None，就 panic 并给出原因
+    };
+    
+    let b = match input.iter().min() {
+        Some(num) => num, // 如果是 Some，就返回里面的值
+        None => panic!("input is empty"), // 如果是 None，就 panic 并给出原因
+    };
 
-    println!("{} is largest and {} is smallest");
+    println!("{} is largest and {} is smallest", a, b);
 }
